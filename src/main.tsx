9a5cb448d870f1d02a6a8a875d397ac7545b913d
Shreyash Patil <107754587+shreyashap@users.noreply.github.com>
2026-04-16 14:28:56 +0530

    add context (#4)
    Co-authored-by: Shreyash <shreyashap>

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { HarvestingProvider } from './context/HarvestingContext.tsx'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <HarvestingProvider>
+        <App />
+      </HarvestingProvider>
+    </TooltipProvider>
   </StrictMode>,
 )
